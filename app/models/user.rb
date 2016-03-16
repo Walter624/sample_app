978872ef45db6056806d1e9416ee77839816f497
Walter624 <WalterRBermudez@gmail.com>
2016-03-16 12:44:26 -0400

    added CSS file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
-class User > ActiveRecord::Base
+class User
   attr_accessor :name, :email
 
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
 end
